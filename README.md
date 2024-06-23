a0163360f404af1a2791357e74f4e2233cbb9137
Thim Cederlund <thim@cederlund.de>
2024-06-23 13:44:56 +0200

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Watch
+
+Watch runs a command each time a set of files changes. Watch is a
+clone of the [9fans/go Watch
+tool](https://github.com/9fans/go/blob/main/acme/Watch/main.go) but
+is made to work outside of Acme.
+
+There are other minor differences between the 9fans/go Watch tool
+and this tool, instead of relying on the current working directory
+the user needs to specify (with globbing support) which files to
+specifically watch. Another difference is the notion of an unescaped
+`%` in the arguments list which is substituted with file path of
+the file that was changed.
+
+Watch may not be as efficient as it could be (using syscalls) but is
+written so that it is as simple and cross-platform as possible.
+
+## Usage
+
+    watch \*.go go run \%
+
+## License
+
+MIT
---
+++ main.go
@@ -0,0 +1,95 @@
+// Command watch runs a command each time a set of file changes.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type metaData struct {
+	Cmd  []string
+	Stat os.FileInfo
+	C    chan struct{}
+}
+
+var durationFlag = flag.Int("d", 1, "delay (in seconds) between each poll")
+
+func watch(filePath string, m metaData) {
+	var dur = time.Second * time.Duration(*durationFlag)
+	for {
+		time.Sleep(dur)
+		s, err := os.Stat(filePath)
+		if err != nil {
+			continue
+		}
+		if s.Size() != m.Stat.Size() ||
+			s.Mode() != m.Stat.Mode() ||
+			s.ModTime() != m.Stat.ModTime() {
+			m.Stat = s
+			m.C <- struct{}{}
+		}
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s pattern cmd [args...]\n", os.Args[0])
+	os.Exit(1)
+}
+
+func main() {
+	var fileMap = make(map[string]metaData)
+	flag.Usage = usage
+	flag.Parse()
+	var args = flag.Args()
+	if len(args) < 2 {
+		usage()
+	}
+	matches, err := filepath.Glob(args[0])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "filepath/Glob: %s\n", err)
+		os.Exit(1)
+	}
+	if len(matches) < 1 {
+		fmt.Fprintf(os.Stderr, "warning: could not match file pattern: %q\n", args[0])
+	}
+	for _, file := range matches {
+		stat, err := os.Stat(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "os/Stat: %q %s\n", file, err)
+			continue
+		}
+		var cmd []string
+		for _, arg := range args[1:] {
+			if arg == "%" {
+				arg = file
+			} else if strings.Contains(arg, "\\%") {
+				arg = strings.Replace(arg, "\\%", "%", -1)
+			}
+			cmd = append(cmd, arg)
+		}
+		fileMap[file] = metaData{
+			Cmd:  cmd,
+			Stat: stat,
+			C:    make(chan struct{}),
+		}
+		go watch(file, fileMap[file])
+	}
+
+	for {
+		for _, file := range fileMap {
+			<-file.C
+			e := exec.Command(file.Cmd[0], file.Cmd[1:]...)
+			e.Stdout = os.Stdout
+			e.Stderr = os.Stderr
+			e.Stdin = os.Stdin
+			if err := e.Run(); err != nil {
+				fmt.Fprintf(os.Stderr, "exec/Command: %s\n", err)
+			}
+		}
+	}
+}
